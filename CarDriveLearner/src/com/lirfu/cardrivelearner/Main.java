10835eb441541589cfeb60ea2516cc45fbfd9594
juraj_fulir <juraj.fulir@fer.hr>
2016-08-04 20:21:50 +0200

    Create class and add .project for easy cloning
    The .project file is needed for Eclipse to recognize the project and
    import it.
    After cloning and importing the project you must configure build path
    library for Java. Then you must add .project to .gitignore so you don't
    have to reconfigure build path everytime you pull the project.

---
+++ CarDriveLearner/src/com/lirfu/cardrivelearner/Main.java
@@ -0,0 +1,19 @@
+package com.lirfu.cardrivelearner;
+
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
+	
+	public static void main(String[] args) {
+		Main myFrame = new Main();
+		myFrame.setSize(1024, 600);
+		myFrame.setLocationRelativeTo(null);
+		myFrame.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE);
+		myFrame.setVisible(true);
+	}
+	
+	public Main() {
+//		add(simulator);
+	}
+}
+
